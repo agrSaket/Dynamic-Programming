42e67964131fa465f29ce1482383a76c25f0e421
Saket <saketagrahari.nita@gmail.com>
2023-07-21 16:38:01 +0530

    added L12, L13

---
+++ L12/L12.cpp
@@ -0,0 +1,19 @@
+// Minimum/Maximum Falling Path Sum
+
+/*
+You have been given an N*M matrix filled with integer numbers, find the maximum sum that can be
+obtained from a path starting from any cell in the first row to any cell in the last row.
+From a cell in a row, you can move to another cell directly below that row, or diagonally below 
+left or right. So from a particular cell (row, col), we can move in three directions i.e.
+Down: (row+l, col)
+Down left diagonal: (row+l, col-l)
+Down right diagonal: (row+l, col+l)
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// driver code 
+int main(){
+        
+}
